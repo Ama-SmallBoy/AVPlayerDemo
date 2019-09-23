21ac5b2c98bcc98e186c63f61a723c817d6fe48c
gaotong zhang <zhanggaotong2@gmail.com>
2019-09-23 15:31:06 +0800

    first commit

---
+++ AVPlayDemo/PlayViewController/PlayerViewController.h
@@ -0,0 +1,17 @@
+//
+//  PlayerViewController.h
+//  AVPlayDemo
+//
+//  Created by a on 2019/9/11.
+//  Copyright © 2019 TeenageBeaconFireGroup. All rights reserved.
+//
+
+#import <AVKit/AVKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PlayerViewController : AVPlayerViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
